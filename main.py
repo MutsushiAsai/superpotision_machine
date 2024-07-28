4e844a0ba96495d6f8e219277522378ceb0b7569
= <=>
2024-07-28 18:04:05 +0900

    add serial control

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import matplotlib as mpl; mpl.use('qtagg'); mpl.rcParams['toolbar'] = 'None' # f
 import matplotlib.pyplot as plt
 import matplotlib.style as mplstyle; mplstyle.use('fast')
 import pyaudio
+import serial
+import struct
+import sys
 
+from collections import namedtuple
 from concurrent.futures import as_completed, ThreadPoolExecutor
 from dataclasses import dataclass
 from scipy import signal
-from random import seed, random
-
-seed(111)
 
 device_count = 4
 
@@ -60,6 +61,32 @@ def synthesize(t: np.array, params: PolygonalSynthesizeParams) -> tuple[np.ndarr
     out_y = fold(fold_ratio * p * np.sin(phi + rotate), -1, 1)
 
     return out_x, out_y
+
+@dataclass
+class ChangeControlFrame:
+    channel: int
+    control_number: int
+    value: int
+
+ports = [
+    "/dev/ttyUSB0",
+    "/dev/ttyUSB1",
+    "/dev/ttyUSB2",
+    "/dev/ttyUSB3",
+]
+
+polygonal_synthesize_params = [
+    PolygonalSynthesizeParams(
+        frequency = 0, # in1: 0 ~ 2000
+        roll = 0, # -1000 ~ 1000
+        n = 2.1,  # 2.1 ~ 100
+        teeth = 0, # 0.0 ~ 1.0
+        fold_ratio = 0, # 
+        fm_ratio = 0, # 
+        fm_modulation = 0, #
+    )
+    for i in range(device_count)
+]
 
 class PolyGonalSynthesisVisualizer:
     def __init__(self, sample_rate: int=44100, synthesizer_count: int = 4, plots_opts: list[dict] = []):
@@ -126,10 +153,78 @@ class PolyGonalSynthesisVisualizer:
         self._figure.canvas.flush_events()
 
 import time, threading
+is_running = False
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+def handle_message(index):
+    global is_running
+    global ports
+    global polygonal_synthesize_params 
+
+    last_connection_status = True
+    try:
+        port = ports[index]
+        params = polygonal_synthesize_params[index]
+
+        def handle_serial(port):
+            with serial.Serial(port, 38400) as ser:
+                print(f'connection established: "{port}"')
+
+                while True:
+                    v = struct.unpack('B', ser.read(1))
+                    if len(v) == 0:
+                        continue
+
+                    v = int(v[0])
+                    if (v & 0xF0) == 0:
+                        continue
+
+                    status = v & 0xF0
+                    channel = v & 0x0F
+
+                    if status != 0xB0:
+                        continue
+
+                    control_number, value = struct.unpack('2B', ser.read(2))
+                    cc = ChangeControlFrame(channel, control_number, value)
+                    if control_number != 0x07: # 0x07: main volume ( midi )
+                        continue
+                    value = cc.value
+                    if cc.channel == 1:
+                        params.frequency = map_range(value, 0, 127, 0, 2000.0)
+                    elif cc.channel == 2:
+                        params.roll = map_range(value, 0, 127, 0, 200.0)
+                    elif cc.channel == 3:
+                        params.n = map_range(value, 0, 127, 2.1, 150.0)
+                    elif cc.channel == 4:
+                        params.teeth =  map_range(value, 0, 127, 0, 0.99)
+                    elif cc.channel == 5:
+                        params.fold_ratio = map_range(value, 0, 127, 0, 100.0) 
+                    elif cc.channel == 6:
+                        params.fm_ratio = map_range(value, 0, 127, 0, 10.0)
+                    elif cc.channel == 7:
+                        params.modulation = map_range(value, 0, 127, 0, 100.0)
+
+        while is_running:
+            try:
+                handle_serial(port)
+
+                last_connection_status = True
+            except serial.serialutil.SerialException as ex:
+                if last_connection_status:
+                    print(f'connection lost: try to connect "{port}"')
+                time.sleep(2)
+                last_connection_status = False
+            except Exception as ex:
+                print(ex)
+
+    except Exception as ex:
+        print(ex)
 
 def main():
-    pa = pyaudio.PyAudio()
-
+    global is_running
     viz = PolyGonalSynthesisVisualizer(SAMPLE_RATE, device_count, plots_opts=[
         {"color":"r", "linewidth": .6},
         {"color":"g", "linewidth": .8},
@@ -137,6 +232,13 @@ def main():
         {"color":"w", "linewidth": .3},
     ])
 
+    is_running = True
+    for i in range(device_count):
+        t = threading.Thread(target=handle_message, args=[i])
+        t.start()
+
+    pa = pyaudio.PyAudio()
+    pool = None
     try:
         streams = {}
         def initializer():
@@ -147,6 +249,7 @@ def main():
                             rate=SAMPLE_RATE,
                             format=pyaudio.paFloat32,
                             output_device_index=1)
+
         pool = ThreadPoolExecutor(max_workers=device_count, initializer=initializer)
         def play(x, y):
             stereo = np.zeros((len(x), 2), dtype=np.float32)
@@ -162,36 +265,14 @@ def main():
         ]
         t = np.linspace(0, 2*np.pi, SAMPLE_RATE, endpoint=False)
  
-        features = []
-
         def _f(i, t, params):
             ret = synthesize(t, params)
             return i, ret
 
         while True:
-            params = [
-                PolygonalSynthesizeParams(
-                    #frequency = 440,  # in1: 0 ~ 2000
-                    #roll = 100, # -1000 ~ 1000
-                    #n = 50,  # 2.1 ~ 100
-                    #teeth = 0.5, # 0.0 ~ 1.0
-                    #fold_ratio = 5, # 
-                    #fm_ratio = 50, # 
-                    #fm_modulation = 0, #
-                    frequency = random()*600+200, # in1: 0 ~ 2000
-                    roll = random()*1000 - 500, # -1000 ~ 1000
-                    n = 2.1 + random() * 47.9,  # 2.1 ~ 100
-                    teeth = random()*5, # 0.0 ~ 1.0
-                    fold_ratio = random()*5, # 
-                    fm_ratio = random()*50, # 
-                    fm_modulation = 0, #
-                )
-                for i in range(device_count)
-            ]
-
             for st, et in time_ranges:
                 features = [
-                    pool.submit( _f, i, t[st:et], params[i])
+                    pool.submit( _f, i, t[st:et], polygonal_synthesize_params[i])
                     for i in range(device_count)
                 ]
 
@@ -207,6 +288,11 @@ def main():
 
     except KeyboardInterrupt:
         pass
+
+    if pool:
+        pool.shutdown(wait=False, cancel_futures=True)
+
+    is_running = False
 
 if __name__ == '__main__':
     main()
